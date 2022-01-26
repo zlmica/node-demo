1bce6fc5baa5dd97c52aafc07bf613b9c1cc3c98
zlmica <zlmica@126.com>
2022-01-26 23:55:08 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const socket = process.env.SOCKET
+const port = socket
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
